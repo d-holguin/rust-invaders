fb752bdbb3f7f369172a63e789e324f17e444f16
dan <d.holguin@protonmail.com>
2022-07-14 07:40:41 -0700

    small changes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+#![allow(unused)] //comment out later
 use bevy::prelude::*;
 
-// region -- asset constants
+// region -- Asset Constants
 const PLAYER_SPRITE: &str = "player_b_01.png";
-const PLAYER_SIZE: (f32, f32) = (144., 75.);
-// endregion -- asset constants
+const PLAYER_SIZE: (f32, f32) = (144.0, 75.0);
+const SPRITE_SCALE: f32 = 0.5;
+// region -- resources
+pub struct WinSize {
+    
+}
 
 fn main() {
     App::new()
@@ -30,12 +35,17 @@ fn setup_system(
     let window = windows.get_primary_mut().unwrap();
     let (win_w, win_h) = (window.width(), window.height());
     // pos for window
-    window.set_position(IVec2::new(2780, 4900));
+    //window.set_position(IVec2::new(2780, 4900));
 
     //add player
-    let bottom = -win_h / 2.;
+    let bottom = -win_h / 2.0;
     commands.spawn_bundle(SpriteBundle {
         texture: asset_server.load(PLAYER_SPRITE),
+        transform: Transform {
+            translation: Vec3::new(0.0, bottom + PLAYER_SIZE.1 / 2.0 * SPRITE_SCALE + 5.0, 10.0),
+            scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.0),
+            ..Default::default()
+        },
         ..Default::default()
     });
 }
