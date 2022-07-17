76c0f73f1e232a939249dab5016101305439b304
Daniel <d.holguin@protonmail.com>
2022-07-17 16:31:08 -0700

    added fireing system, lasers, basic enemy

---
+++ src/enemy.rs
@@ -0,0 +1,16 @@
+use crate::GameTextures;
+use bevy::prelude::*;
+
+pub struct EnemyPlugin;
+
+impl Plugin for EnemyPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system_to_stage(StartupStage::PostStartup, enemy_spawn_system);
+    }
+}
+fn enemy_spawn_system(mut commands: Commands, game_textures: Res<GameTextures>) {
+    commands.spawn_bundle(SpriteBundle {
+        texture: game_textures.enemy.clone(),
+        ..Default::default()
+    });
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,40 @@
 #![allow(unused)] //comment out later
 use bevy::prelude::*;
+use components::{Movable, Velocity};
+use player::PlayerPlugin;
+use enemy::EnemyPlugin;
 
-// region -- Asset Constants
+mod components;
+mod enemy;
+mod player;
+// region: Asset Constants
 const PLAYER_SPRITE: &str = "player_b_01.png";
+const PLAYER_LASER_SPRITE: &str = "laser_a_01.png";
+const PLAYER_LASER_SIZE: (f32, f32) = (9.0, 54.0);
 const PLAYER_SIZE: (f32, f32) = (144.0, 75.0);
+//enemy
+const ENEMY_SPRITE: &str = "enemy_a_01.png";
+const ENEMY_SIZE: (f32, f32) = (144.0, 75.0);
+const ENEMY_LASER_SPRITE: &str = "laser_b_01.png";
+const ENEMY_LASER_SIZE: (f32, f32) = (17.0, 55.0);
+
 const SPRITE_SCALE: f32 = 0.5;
-// region -- resources
+// region: Game constant
+const TIME_STEP: f32 = 1.0 / 60.0;
+const BASE_SPEED: f32 = 500.0;
+// region: Resources
 pub struct WinSize {
     pub w: f32,
     pub h: f32,
 }
-struct GameTextures{
+
+struct GameTextures {
     player: Handle<Image>,
+    player_laser: Handle<Image>,
+    enemy: Handle<Image>,
+    enemy_laser: Handle<Image>,
 }
-// region -- end resources
+// endregion: resources
 
 fn main() {
     App::new()
@@ -25,8 +46,10 @@ fn main() {
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
+        .add_plugin(PlayerPlugin)
+        .add_plugin(EnemyPlugin)
         .add_startup_system(setup_system)
-        .add_startup_system_to_stage(StartupStage::PostStartup, player_spawn_system)
+        .add_system(movable_system)
         .run();
 }
 
@@ -49,23 +72,31 @@ fn setup_system(
     // adding GameTextures Resource
     let game_textures = GameTextures {
         player: asset_server.load(PLAYER_SPRITE),
+        player_laser: asset_server.load(PLAYER_LASER_SPRITE),
+        enemy: asset_server.load(ENEMY_SPRITE),
+        enemy_laser: asset_server.load(ENEMY_LASER_SPRITE),
     };
     commands.insert_resource(game_textures);
 }
-fn player_spawn_system(
+fn movable_system(
     mut commands: Commands,
-    game_textures: Res<GameTextures>,
     win_size: Res<WinSize>,
+    mut query: Query<(Entity, &Velocity, &mut Transform, &Movable)>,
 ) {
-    //add player
-    let bottom = -win_size.h / 2.0;
-    commands.spawn_bundle(SpriteBundle {
-        texture: game_textures.player.clone(),
-        transform: Transform {
-            translation: Vec3::new(0.0, bottom + PLAYER_SIZE.1 / 2.0 * SPRITE_SCALE + 5.0, 10.0),
-            scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.0),
-            ..Default::default()
-        },
-        ..Default::default()
-    });
+    for (entity, velocity, mut transform, movable) in query.iter_mut() {
+        let translation = &mut transform.translation;
+        translation.x += velocity.x * TIME_STEP * BASE_SPEED;
+        translation.y += velocity.y * TIME_STEP * BASE_SPEED;
+
+        if movable.auto_despawn {
+            const MARGIN: f32 = 200.0;
+            if translation.y > win_size.h / 2.0 + MARGIN
+                || translation.y < -win_size.h / 2.0 - MARGIN
+                || translation.x > win_size.w / 2.0 + MARGIN
+                || translation.x < -win_size.w / 2.0 - MARGIN
+            {
+                commands.entity(entity).despawn();
+            }
+        }
+    }
 }
