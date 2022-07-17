7d127e7420a40c5fa5a4e2a2dae6f3ca6f644499
Dantes H <d.holguin@protonmail.com>
2022-07-16 21:18:55 -0700

    added spawn player system

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,10 @@ const PLAYER_SIZE: (f32, f32) = (144.0, 75.0);
 const SPRITE_SCALE: f32 = 0.5;
 // region -- resources
 pub struct WinSize {
-    
+    pub w: f32,
+    pub h: f32,
 }
+// region -- end resources
 
 fn main() {
     App::new()
@@ -19,8 +21,9 @@ fn main() {
             height: 676.0,
             ..Default::default()
         })
-        .add_system(setup_system)
         .add_plugins(DefaultPlugins)
+        .add_startup_system(setup_system)
+        .add_startup_system_to_stage(StartupStage::PostStartup, player_spawn_system)
         .run();
 }
 
@@ -36,9 +39,16 @@ fn setup_system(
     let (win_w, win_h) = (window.width(), window.height());
     // pos for window
     //window.set_position(IVec2::new(2780, 4900));
-
+    let win_size = WinSize { w: win_w, h: win_h };
+    commands.insert_resource(win_size);
+}
+fn player_spawn_system(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    win_size: Res<WinSize>,
+) {
     //add player
-    let bottom = -win_h / 2.0;
+    let bottom = -win_size.h / 2.0;
     commands.spawn_bundle(SpriteBundle {
         texture: asset_server.load(PLAYER_SPRITE),
         transform: Transform {
